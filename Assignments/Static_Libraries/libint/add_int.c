7246d4257407c4a415db1feca212e45fcf82808c
Elijah Guzman <portalplaya02@gmail.com>
2024-02-01 15:13:54 -0600

    Working on part 1 of assignment 2

---
+++ Assignments/Static_Libraries/libint/add_int.c
@@ -0,0 +1,8 @@
+// Elijah Guzman - NZE594
+// Date: 02/01/2024
+// Description: Basic Addition program for ONLY INT data types.
+
+
+int add_int(int a, int b) {
+    return a + b;
+}
---
+++ Assignments/Static_Libraries/libint/int_arithmetic.h
@@ -0,0 +1,22 @@
+// Elijah Guzman - NZE594
+// Date: 02/01/2024
+// Description: Header file for declaring the functions for libint library
+
+#include <stdio.h>
+
+int add_int(int a, int b){
+    return a + b;
+}
+int sub_int(int a, int b){
+    return a - b;
+}
+int mul_int(int a, int b){
+    return a * b;
+}
+int div_int(int a, int b){
+    if (b == 0){
+        return 0;
+    }
+
+    return a / b;
+}
