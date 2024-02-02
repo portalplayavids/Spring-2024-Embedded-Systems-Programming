5c901207cf159f224950592dd7562e8588d897fb
Elijah Guzman <portalplaya02@gmail.com>
2024-02-01 19:32:45 -0600

    Worked on Assignment 2: setting up the files

---
+++ Assignments/Static_Libraries/libdouble/add_double.cpp
@@ -0,0 +1,9 @@
+// Elijah Guzman - NZE594
+// Date: 02/01/2024
+// Description: Basic Addition program for ONLY DOUBLE data types.
+
+#include "double_arithmetic.h"
+
+double add_double(double a, double b) {
+    return a + b;
+}
---
+++ Assignments/Static_Libraries/libdouble/double_arithmetic.h
@@ -0,0 +1,11 @@
+// Elijah Guzman - NZE594
+// Date: 02/01/2024
+// Description: Header file for declaring the functions for libdouble library
+
+double add_double(double a, double b);
+
+double sub_double(double a, double b);
+
+double mul_double(double a, double b);
+
+double div_double(double a, double b);
